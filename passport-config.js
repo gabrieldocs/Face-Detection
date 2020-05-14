ecce0ab95087f9316a93b0288b8d6d9100bb1ba1
gabrieldocs <mydocs.lucas@gmail.com>
2020-05-14 14:35:16 -0300

    Upload

---
+++ passport-config.js
@@ -0,0 +1,31 @@
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcrypt')
+
+function initialize(passport){
+    const authenticateUser = (email, password, done) =>{
+        const user = getUserByEmail(email)
+        if(user == null) {
+            return done(null, false, { message: 'No user with that email'})
+        } 
+
+        try{
+            if(await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, {message : 'Password incorrect'})
+            }
+        } catch(err) {
+            return done(err)
+        }
+    }
+
+    passport.use(new LocalStrategy({
+        usernameField: 'email'
+    }), authenticateUser)
+
+    passport.serializeUser((user, done) => { })
+    passport.deserializeUser((id, done) => { })
+}
+
+
+module.exports = initialize
