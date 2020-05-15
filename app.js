d4d67b85b56477e37459a4536e0388df8dc34b12
gabrieldocs <mydocs.lucas@gmail.com>
2020-05-15 16:20:03 -0300

    Adding up Sequelize and Stuff

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 
 app.set('view-engine', 'ejs')
@@ -6,9 +7,26 @@ app.use(express.static('assets'))
 app.use(express.urlencoded({ extended: false}))
 
 
+const user = []
+
+app.get('/register', (req, res)=>{
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+app.post('/register', (req, res)=>{
+    res.send('Cadastro recebido com sucesso')
+    user.push({nome: req.body.nome, rg : req.body.rg})
+    
+    //fs.mkdirSync(req.body.rg)
+    
+})
+
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/views/index.html')
 })
-app.listen(4000, ()=>{
-    console.log('Something')
+app.listen(8080, ()=>{
+    console.log('Sample')
+    //setInterval(()=>{
+    //    console.log(__dirname + ' File '+ __filename)
+    //}, 1000)
 })
